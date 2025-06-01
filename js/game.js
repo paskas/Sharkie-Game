8d3dcf9954e0fb2a5e16a0507d7e62ae5563d0e8
paskas <pascal.kasbeitzer@gmail.com>
2025-06-01 12:25:24 +0200

    browser fullscreen mode added

--- js/game.js
+++ js/game.js
@@ -7,3 +7,26 @@ function init() {
   initLevel();
   world = new World(canvas, keyboard);
 }
+
+function fullscreen(){
+  let fullscreen = document.getElementById('gameContainer');
+  enterFullscreen(fullscreen);
+}
+
+function enterFullscreen(container) {
+  if(container.requestFullscreen) {
+    container.requestFullscreen();
+  } else if(container.msRequestFullscreen) {
+    container.msRequestFullscreen();
+  } else if(container.webkitRequestFullscreen) {
+    container.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
